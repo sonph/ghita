771282bc4e6c67bfa1678c4e944cda2f693cd386
Son A. Pham <sonph@users.noreply.github.com>
2018-07-06 22:01:31 -0700

    Add teoria and simple root note picker (#4)
    * Add teoria.js
    * Example to bind on click and css class

--- app.py
+++ app.py
@@ -1,15 +1,43 @@
-class App:
+import note
+
+
+def join(sep, items):
+  s = ''
+  for item in items:
+    s += sep + str(item)
+  return s
+
+
+class SelectedRoot(object):
   def __init__(self):
-    pass
+    self.selectedRoot = 'A'
+    self.scale = self.getMajorScaleStr(self.selectedRoot)
+
+  def selectRoot(self, rootNote: str):
+    console.log('selected ' + rootNote)
+    self.selectedRoot = rootNote
+    self.scale = self.getMajorScaleStr(self.selectedRoot)
+
+  @staticmethod
+  def getMajorScaleStr(note) -> str:
+    root = teoria.note(note)
+    notes = root.scale('ionian').simple()
+    return join(' ', notes)
+
+class App(object):
+  def __init__(self):
+    self.selectedRoot = SelectedRoot()
 
   def start(self) -> None:
     console.log('python start()')
     self.initVue()
 
-  def getMessage(self) -> str:
-    return '{0}'.format('Hello world!')
-
   def initVue(self) -> None:
-    app = __new__(Vue({'el': '#app', 'data': {'message': self.getMessage()}}))
+    window.app = __new__(Vue({
+      'el': '#app',
+      'data': {
+        'selectedRoot': self.selectedRoot
+      },
+    }))
 
 window.addEventListener('load', lambda: App().start())
