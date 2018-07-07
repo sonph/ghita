0be805a8cba2b834a5d9e4caea01c97a660d0807
Son A. Pham <sonph@users.noreply.github.com>
2018-07-06 19:57:25 -0700

    example to bind on click and css class

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 import note
 
-class App:
-  def __init__(self):
-    pass
 
-  def start(self) -> None:
-    console.log('python start()')
-    self.initVue()
+class SelectedRoot(object):
+  def __init__(self):
+    self.isCSelected = True
+    self.isDSelected = False
+
+  def selectNote(self, note):
+    console.log('selected ' + note)
+    if note == 'C':
+      self.isCSelected = True
+      self.isDSelected = False
+    elif note == 'D':
+      self.isCSelected = False
+      self.isDSelected = True
+
+
+class App(object):
+  def __init__(self):
+    self.selectedRoot = SelectedRoot()
 
   def getMajorScale(self, note):
     return note.scale('ionian').simple()
@@ -14,10 +26,26 @@ class App:
   def notesToStr(self, notes):
     return ' '.join(notes)
 
-  def initVue(self) -> None:
+  def getMessage(self):
     message = ''
     for n in note.ALL_NOTES:
       message += self.notesToStr(self.getMajorScale(n)) + '\n'
-    app = __new__(Vue({'el': '#app', 'data': {'message': message}}))
+    return message
+
+  def start(self) -> None:
+    console.log('python start()')
+    self.initVue()
+
+  def initVue(self) -> None:
+    window.app = __new__(Vue({
+      'el': '#app',
+      'data': {
+        'message': self.getMessage(),
+        'selectedRoot': self.selectedRoot
+      },
+      'methods': {
+        'selectNote': self.selectedRoot.selectNote,
+      },
+    }))
 
 window.addEventListener('load', lambda: App().start())
