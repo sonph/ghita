f7caadbe68775fe6de9ce312ef70f0569e4c5430
Son A. Pham <sonph@users.noreply.github.com>
2018-07-11 02:00:46 -0700

    Add option panel and constants for ukulele (#29)

--- app_config.py
+++ app_config.py
@@ -2,10 +2,13 @@ class AppConfig:
   def __init__(self):
     # If true, show simple constants.CHORDS only.
     # Otherwise show all available chords in Tonal.
-    self.simple_chords = True
+    self.simple_chords = True  # type: bool
 
     # If true, settings panel is visible
-    self.open_settings = False
+    self.open_settings = False  # type: bool
+
+    # Selected instrument. 'guitar' or 'ukulele'
+    self.instrument = 'guitar'  # type: str
 
   def toggleSettings(self):
     self.open_settings = not self.open_settings
